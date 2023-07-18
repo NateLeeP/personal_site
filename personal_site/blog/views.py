ca922438b2f23da0157690a13d6a620847f2270e
Nate Pruitt <nlpruitt@gmail.com>
2023-07-18 04:59:24 -0500

    First template in project. Used template inheritance, 'url' built-in template tag

--- personal_site/blog/views.py
+++ personal_site/blog/views.py
@@ -2,17 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Posts
 
+# Render does 3 in 1 - loads template, fills context, and retuns an
+# HTTPResponse. You NEED to retun an HTTPResponse for each Django view.
+from django.shortcuts import render
+
 # Create your views here.
 
 
 def index(request):
     content = Posts.objects.all()
-    return HttpResponse(f"There are {len(content)} number of posts")
+    return render(request, "blog/index.html", context={"content": content})
 
 
 def post_page(request, id):
     content = Posts.objects.get(id=id)
 
     return HttpResponse(
-        f"You're looking at post {id} with content {content.post} written by {content.author.name}"
+        f"You're looking at post {id} with content {content.post_body} written by {content.author.name}"
     )
