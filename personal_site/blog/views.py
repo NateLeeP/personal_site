64e66ef6ed859142c8c28bf88b239c788c3d2883
Nate P <nlpruitt@gmail.com>
2023-07-26 06:04:15 -0500

    Rewrite new_post_page view to save blog post to DB via form

--- personal_site/blog/views.py
+++ personal_site/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Post
+from .models import Post, Author
 
 # Render does 3 in 1 - loads template, fills context, and retuns an
 # HTTPResponse. You NEED to retun an HTTPResponse for each Django view.
@@ -24,6 +24,12 @@ def new_post_page(request):
     print(request)
     
     if request.method == 'POST':
+        post = Post()
+        author = Author.objects.get(id=1)
+        post.post_body = request.POST['blog-body']
+        post.post_title = request.POST['blog-title']
+        post.author = author
+        post.save()
         print(request.POST)
         print(request.POST['blog-body'])
     return render(request, "blog/new_post.html")
