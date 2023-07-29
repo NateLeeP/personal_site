016359567d375b946cdd6ff456b8abe0c06d98b7
Nate P <nlpruitt@gmail.com>
2023-07-29 18:48:18 -0500

    Allow admin to edit posts / be able to navigate to create post from blog home

--- personal_site/blog/urls.py
+++ personal_site/blog/urls.py
@@ -1,12 +1,13 @@
-from django.urls import path, include
+from django.urls import path
 
-from .views import index, post_page, new_post_page
+from .views import index, post_page, create_post, edit_post
 
 # namespace your urls, incase a different app uses 'new-post-page
 app_name = 'blog'
 
 urlpatterns = [
     path("", index, name="home"),
-    path("post", new_post_page, name="new-post-page"),
+    path("create_post", create_post, name="create-post"),
     path("post/<int:id>", post_page, name="post-page"),
+    path("edit_post/<int:id>", edit_post, name="edit-post")
 ]
--- personal_site/blog/views.py
+++ personal_site/blog/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Post, Author, PostForm
-
+from .models import Post, PostForm
+from django.contrib.auth.decorators import login_required
 # Render does 3 in 1 - loads template, fills context, and retuns an
 # HTTPResponse. You NEED to retun an HTTPResponse for each Django view.
 from django.shortcuts import render
@@ -11,25 +10,35 @@ from django.shortcuts import render
 
 def index(request):
     content = Post.objects.all()
-    return render(request, "blog/index.html", context={"content": content})
+    is_authenticated = request.user.is_authenticated
+    return render(request, "blog/index.html", context={"content": content, "is_authenticated": is_authenticated})
 
 
 def post_page(request, id):
     post = Post.objects.get(id=id)
+    is_authenticated = request.user.is_authenticated
+    return render(request, "blog/post.html", {"post": post, "is_authenticated": is_authenticated, 'post_id': id})
 
-    return render(request, "blog/post.html", {"post": post})
-
-
-def new_post_page(request):
+@login_required
+def create_post(request):
     if request.method == "GET":
-        post = Post.objects.get(pk=1)
-        form = PostForm(instance=post)
+        form = PostForm()
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
-            print('Form is valid')
             form.save()
         else:
             print('Nope, not valid')
         print(request.POST)
-    return render(request, "blog/new_post.html", {'form': form})
+    return render(request, "blog/create_post.html", {'form': form})
+
+@login_required
+def edit_post(request, id):
+    post_obj = Post.objects.get(pk=id)
+    if request.method == "GET":
+        form = PostForm(instance=post_obj)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post_obj)
+        if form.is_valid():
+            form.save()
+    return render(request, "blog/create_post.html", {'form': form})
