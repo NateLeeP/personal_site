82c712cd905d7ecd8f759e85e39008030b29618b
Nate Pruitt <nlpruitt@gmail.com>
2023-10-31 06:27:59 -0500

    Fix typo in settings

--- personal_site/personal_site/settings.py
+++ personal_site/personal_site/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.envrion.get("ALLOWED_HOSTS", [])
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", [])
 
 
 # Application definition
