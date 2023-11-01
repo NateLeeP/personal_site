9540179859f68dbfb418e516e13047c7dc8c3772
Nate Pruitt <nlpruitt@gmail.com>
2023-11-01 07:54:18 -0500

    Refactored url path for upload images

--- personal_site/blog/urls.py
+++ personal_site/blog/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path("create_post", create_post, name="create-post"),
     path("post/<int:id>", post_page, name="post-page"),
     path("edit_post/<int:id>", edit_post, name="edit-post"),
-    path("upload_image", upload_image, name="upload-image"),
+    path("edit_post/upload_image", upload_image, name="upload-image"),
     path("chatgpt", chatgpt_page, name="chat-gpt"),
     path("chatgpt_post", chatgpt_post, name="chat-gpt-post"),
 ]
